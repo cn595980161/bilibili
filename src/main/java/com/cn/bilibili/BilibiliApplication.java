cc476b6a47a144319cb50897f8f7f9508c8605a3
cn <595980161@qq.com>
2017-12-11 11:02:53 +0800

    base

---
+++ src/main/java/com/cn/bilibili/BilibiliApplication.java
@@ -0,0 +1,12 @@
+package com.cn.bilibili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilibiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilibiliApplication.class, args);
+	}
+}
